1ab478432518769044a3683d708ca20c458a08d8
h4ckery.b1nx <51971090+zbinx@users.noreply.github.com>
2023-04-08 08:37:19 -0400

    update selen.py

--- selen.py
+++ selen.py
@@ -1,5 +1,4 @@
 #!/bin/usr/python3
-# Using python3 3.8.10
 # Using Chrome version 112 with ChromeDriver 112.0.5615.49
 
 # import all the necessary modules and packages
@@ -9,6 +8,9 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 import time
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 # create the options variable to avoid errors & to add options like headless or window size.
 options = webdriver.ChromeOptions()
@@ -45,9 +47,32 @@ html = driver.page_source
 
 # Create if else statement to see if the *** signifier shows up indicating out of stock.
 if '***' in html:
-    print('PRODUCT UNAVAILABLE')
+    result = ("PRODUCT UNAVAILABLE")
+    print(result)
 else:
-    print('IN STOCK AND AVAILABLE')
+    result = ("IN STOCK")
+    print(result)
     
+# Create the email message
+msg = MIMEMultipart()
+msg['From'] = 'User@gmail.com'
+msg['To'] = 'User@gmail.com'
+msg['Subject'] = result
+
+# Add the scraped result to the email message
+body = str(result)
+msg.attach(MIMEText(body, str))
+
+# Set up the SMTP server and send the email.
+smtp_server = 'smtp.gmail.com'
+smtp_port = 587
+smtp_username = 'User@gmail.com'
+smtp_password = 'app-password-here'  # If using gmail, need to use App passwords to retrieve this.
+smtp_connection = smtplib.SMTP(smtp_server, smtp_port)
+smtp_connection.starttls()
+smtp_connection.login(smtp_username, smtp_password)
+smtp_connection.sendmail(msg['From'], msg['To'], msg.as_string())
+smtp_connection.quit()
+
 #print(driver.page_source)
 driver.quit()
